4fa79f0938c02ff5daa64d44da30fdcae88b1bd1
sanjaybaskaran01 <sanjaybaskaran01@gmail.com>
2021-05-13 01:08:10 +0530

    Finished Lesson4

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const http=require('http');
 const fs=require('fs');
+const _ = require('lodash');
+
 
 const server=http.createServer((req,res)=>{
-    console.log(req.url,req.method);
+
+    //lodash
+    const num=_.random(0,20);
+    console.log(num);
+
+    const greet =_.once(()=>{
+        console.log("Hello");
+    });
+    greet();
     //set header content type
     res.setHeader('Content-Type','text/html');
     
@@ -17,7 +27,7 @@ const server=http.createServer((req,res)=>{
             path+='about.html';
             res.statusCode=200;
             break;
-        case '/about-me':
+        case '/about-us':
             res.statusCode=301;
             res.setHeader('location','/about');
             res.end();
