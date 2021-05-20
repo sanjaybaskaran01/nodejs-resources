6541152c0a553f3bb04ee1637dca4aa8aee2f861
sanjaybaskaran01 <sanjaybaskaran01@gmail.com>
2021-05-20 23:18:39 +0530

    Finished lesson 6

--- app.js
+++ app.js
@@ -3,29 +3,33 @@ const express = require('express')
 //express app
 
 const app = express();
+//creating a new engine
+
+app.set('view engine','ejs');
 
 //listen for requests
 app.listen(3000);
 
 app.get('/',(req,res)=>{
-    // res.send('This is the home page');
-    res.sendFile('./views/index.html',{ root: __dirname });
+    const blogs =[
+        {title:'Yoshi finds eggs',snippet:'Lorem ipsum dolor sit amet'},
+        {title:'Mario finds stars',snippet:'Lorem ipsum dolor sit amet'},
+        {title:'How to defeat bowser',snippet:'Lorem ipsum dolor sit amet'},
+    ]
+    res.render('index',{title:"Home",blogs});
 });
 
 app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html',{ root: __dirname });
-
+    res.render('about',{title:"About"})
 });
 
-//redirects
+app.get('/blogs/create',(req,res)=>{
+    res.render('create',{title:"Create a blog"})
 
-app.get('/about-us',(req,res)=>{
-    res.redirect('/about');
 })
-
 //404 page
 
 app.use((req,res)=>{
-    res.status(404).sendFile("./views/404.html",{root:__dirname});
+    res.status(404).render('404',{title:"404 Error"});
 
 })
